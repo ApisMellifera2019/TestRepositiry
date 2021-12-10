10394bca6a45c7cbc9b44b0849c5b250bcd05054
Andrew Usanin <AndreyUS@users.noreply.github.com>
2021-12-10 11:01:46 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@ package AndrLess;
 
 public class Main {
     public static void main(String[] args) {
+        //Програмируй на уровне интерфейсов
         Rand rand = new Rand();
+        // RandomNumbers rand = new Rand();
         rand.printingNumbers(rand.numberOfRandomDigits());
     }
 }
